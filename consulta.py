0254155a32e984f0e70a870d841a099851e1f274
Rafa Lourenço <52457167+Rafasputnick@users.noreply.github.com>
2022-11-26 13:44:37 -0300

    feat: create a basic classifier

--- consulta.py
+++ consulta.py
@@ -4,117 +4,89 @@
 
 import os
 from neo4j import GraphDatabase
+from movie import Movie
 
 linked_movies = {}
 
-class Neo4jConnection:
-  def __init__(self, uri, user, pwd):
-    self.__uri = uri
-    self.__user = user
-    self.__pwd = pwd
-    self.__driver = None
-    try:
-        self.__driver = GraphDatabase.driver(self.__uri, auth=(self.__user, self.__pwd))
-    except Exception as e:
-        print("Failed to create the driver:", e)
-      
-  def close(self):
-    if self.__driver is not None:
-        self.__driver.close()
-      
-  def query(self, query, parameters=None, db=None):
-    assert self.__driver is not None, "Driver not initialized!"
-    session = None
-    response = None
-    try: 
-      session = self.__driver.session(database=db) if db is not None else self.__driver.session() 
-      response = list(session.run(query, parameters))
-    except Exception as e:
-      print("Query failed:", e)
-    finally: 
-      if session is not None:
-        session.close()
-    return response
 
-conn = Neo4jConnection(uri=os.environ['NEO4J_URL'], 
-                       user="neo4j",              
+class Neo4jConnection:
+    def __init__(self, uri, user, pwd):
+        self.__uri = uri
+        self.__user = user
+        self.__pwd = pwd
+        self.__driver = None
+        try:
+            self.__driver = GraphDatabase.driver(
+                self.__uri, auth=(self.__user, self.__pwd))
+        except Exception as e:
+            print("Failed to create the driver:", e)
+
+    def close(self):
+        if self.__driver is not None:
+            self.__driver.close()
+
+    def query(self, query, parameters=None, db=None):
+        assert self.__driver is not None, "Driver not initialized!"
+        session = None
+        response = None
+        try:
+            session = self.__driver.session(
+                database=db) if db is not None else self.__driver.session()
+            response = list(session.run(query, parameters))
+        except Exception as e:
+            print("Query failed:", e)
+        finally:
+            if session is not None:
+                session.close()
+        return response
+
+
+conn = Neo4jConnection(uri=os.environ['NEO4J_URL'],
+                       user="neo4j",
                        pwd=os.environ['NEO4J_PW'])
 
-def consultar(query, resType: str):
-  response = []
-  for line in conn.query(query):
-        if resType == 'dict':
-            response.append(dict(line)['relatedMovie'])
-        elif resType == 'str':
-            response.append(dict(line)['relatedMovie'])
-  return response
 
-def search_top_related_movies(movieId: str, how_much_movies: str):
+def updateRelatedPoints(query, linked_movies: dict, points: float):
+    for line in conn.query(query):
+        response = dict(line)
+        mId = response['rMovieId']
+        movie = Movie(response['rMovieTitle'], response['rMovieUrl'])
+        if linked_movies.get(mId) == None:
+            linked_movies[mId] = movie
+
+        linked_movies[mId].multiplyPoints(points)
+
+
+def search_top_related_movies(movieId: str, how_much_movies: int):
     linked_movies = {}
-    
-    print("Filmes com o(s) mesmo(s) genero(s)")
-    response = consultar(f"""
+
+    # Filmes com o(s) mesmo(s) genero(s)
+    updateRelatedPoints(f"""
     MATCH (curMovie:Movie)-[:IN_GENRE]->(g:Genre)<-[:IN_GENRE]-(relatedMovie:Movie)
     WHERE curMovie.movieId = '{movieId}'
-    RETURN relatedMovie.movieId as relatedMovie
-    """, "str")
-    for mId in response:
-        if linked_movies.get(mId) != None:
-            linked_movies[mId] += 5
-        else:
-            linked_movies[mId] = 5
+    RETURN relatedMovie.movieId as rMovieId, relatedMovie.title as rMovieTitle, relatedMovie.url as rMovieUrl
+    """, linked_movies, 1.2)
 
-    print()        
+    # Filmes com o mesmo diretor
+    updateRelatedPoints(f"""
+    MATCH (curMovie:Movie)<-[:DIRECTED]-(d:Director)-[:DIRECTED]->(relatedMovie:Movie)
+    WHERE curMovie.movieId = '{movieId}'
+    RETURN relatedMovie.movieId as rMovieId, relatedMovie.title as rMovieTitle, relatedMovie.url as rMovieUrl
+    """, linked_movies, 1.4)
 
-search_top_related_movies('614', '5')
+    # Filmes com o(s) mesmo(s) atore(s)/atriz(es)
+    updateRelatedPoints(f"""
+    MATCH (curMovie:Movie)<-[:ACTED_IN]-(a:Actor)-[:ACTED_IN]->(relatedMovie:Movie)
+    WHERE curMovie.movieId = '{movieId}'
+    RETURN relatedMovie.movieId as rMovieId, relatedMovie.title as rMovieTitle, relatedMovie.url as rMovieUrl
+    """, linked_movies, 1.6)
 
-# print("Filme e genero")
-# consultar("""
-# MATCH (m:Movie)-[:IN_GENRE]->(g:Genre)<-[:IN_GENRE]-(m2:Movie)
-# WITH m.title AS nome1, g.name as genero, m2.title as nome2
-# WHERE m.movieId = '614' and g.name <> 'Thriller'
-# RETURN nome1, genero, nome2
-# """, "str")
-
-# print("Pegando um filme aleatorio")
-# consultar("""
-# MATCH (m:Movie)
-# WITH m.movieId as movieId, m.title AS nome, rand() as randNumber
-# order by randNumber
-# limit 1
-# RETURN movieId, nome
-# """)
+    top_related_movies = sorted(list(linked_movies.values()), key=lambda value: value.related_points, reverse=True)
+    return top_related_movies[:how_much_movies]
 
 
-# print("Pegando um filme especifico")
-# consultar("""
-# MATCH (m:Movie)
-# WITH m.movieId as idImdb, m.title AS nome, m.year as year, m.url as url
-# WHERE m.imdbId = '0110374'
-# RETURN idImdb, nome, year, url
-# """)
+top_movies = search_top_related_movies('614', 5)
 
-
-# print("DISCIPLINA COM MAIOR NÚMERO DE INSCRIÇÕES")
-# consultar("""
-# MATCH (d:DISCIPLINA)<-[i:INSCRITO]-()
-# WITH count(d.nome) as counter, d.nome as nome
-# order by counter desc
-# LIMIT 1
-# RETURN nome
-# """)
-
-# print("ALUNOS POR SALA DE AULA")
-# consultar("""
-# MATCH  (e:ESTUDANTE)-[:INSCRITO]->(d:DISCIPLINA)-[:ALOCADO]->(s:SALA)
-# WITH count(s.nome) as counter, s.nome as nome, s.capacidade as cap
-# RETURN nome, ((counter * 100) / cap) + '%' as porcentagem
-# """)
-
-# print("Quantidade de alunos por blocos")
-# consultar("""
-# MATCH  (e:ESTUDANTE)-[:INSCRITO]->(d:DISCIPLINA)-[:ALOCADO]->(s:SALA)
-# WITH count(s.nome) as counter, s.bloco as bloco
-# order by bloco asc
-# RETURN bloco, counter
-# """)
+print("Filmes recomendados:")
+for i, movie in enumerate(top_movies):
+  print(f'{i + 1}º - {movie.title}')
